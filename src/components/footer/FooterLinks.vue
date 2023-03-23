c6a11725b25dfc66fd47b31033aae2f4082f8757
Gaspare <Gas9220@users.noreply.github.com>
2023-03-23 18:12:39 +0100

    Completata parte superiore footer

--- src/components/footer/FooterLinks.vue
+++ src/components/footer/FooterLinks.vue
@@ -15,6 +15,26 @@ export default {
             shopLinks: [
                 "Shop DC",
                 "Shop DC Collectibles"
+            ],
+            dcLinks: [
+                "Terms Of Use",
+                "Privacy Policy (New)",
+                "Ad Choices",
+                "Advertising",
+                "Jobs",
+                "Subscriptions",
+                "Talent Workshops",
+                "CPSC Certificates",
+                "Ratings",
+                "Shop Help",
+                "Contact Us"
+            ],
+            sitesLinks: [
+                "DC",
+                "MAD Magazine",
+                "DC Kids",
+                "DC Universe",
+                "DC Power Visa"
             ]
         }
     }
@@ -22,38 +42,72 @@ export default {
 </script>
 
 <template>
-    <div>
-        <h3>DC COMICS</h3>
-        <ul>
-            <li v-for="link in dcComicsLinks"><a href="#"> {{ link }}</a></li>
-        </ul>
+    <div class="links">
+        <div class="left">
+            <div>
+                <h3>DC COMICS</h3>
+                <ul>
+                    <li v-for="link in dcComicsLinks"><a href="#"> {{ link }}</a></li>
+                </ul>
+            </div>
 
-        <h3>SHOP</h3>
-        <ul>
-            <li v-for="link in shopLinks"><a href="#"> {{ link }}</a></li>
-        </ul>
+            <div>
+                <h3>SHOP</h3>
+                <ul>
+                    <li v-for="link in shopLinks"><a href="#"> {{ link }}</a></li>
+                </ul>
+            </div>
+
+        </div>
+        <div class="right">
+            <div>
+                <h3>DC</h3>
+                <ul>
+                    <li v-for="link in dcLinks"><a href="#"> {{ link }}</a></li>
+                </ul>
+            </div>
+            <div>
+                <h3>SHOP</h3>
+                <ul>
+                    <li v-for="link in shopLinks"><a href="#"> {{ link }}</a></li>
+                </ul>
+            </div>
+
+        </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
-div {
-    padding: .5rem;
+div.links {
+    display: flex;
 
-    h3 {
-        color: white;
-        margin-top: 15px;
+    div.right {
+        display: flex;
     }
 
-    ul {
-        list-style-type: none;
-        margin-top: .5rem;
+    div {
 
-        li {
-            margin: .3125rem 0;
 
-            a {
-                text-decoration: none;
-                color: gray;
+        h3 {
+            color: white;
+            margin-top: 15px;
+        }
+
+        ul {
+            list-style-type: none;
+            margin-top: .5rem;
+
+            li {
+                margin: .3125rem 0;
+
+                a {
+                    text-decoration: none;
+                    color: gray;
+
+                    &:hover {
+                        color: white;
+                    }
+                }
             }
         }
     }
