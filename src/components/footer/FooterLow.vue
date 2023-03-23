276d8041ac27bd0ee4bf17841a9b31e72e2271f6
Gaspare <Gas9220@users.noreply.github.com>
2023-03-23 18:38:15 +0100

    Completati link social

--- src/components/footer/FooterLow.vue
+++ src/components/footer/FooterLow.vue
@@ -1,7 +1,11 @@
 <script>
+import FooterSocialIcons from './FooterSocialIcons.vue';
 
 export default {
-    name: "FooterLow"
+    name: "FooterLow",
+    components: {
+        FooterSocialIcons
+    }
 }
 </script>
 
@@ -9,6 +13,7 @@ export default {
     <section>
         <div class="container">
             <a href="#">SIGN-UP NOW!</a>
+            <FooterSocialIcons></FooterSocialIcons>
         </div>
     </section>
 </template>
@@ -20,12 +25,21 @@ section {
     div.container {
         padding: 30px 0;
 
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
         a {
             display: inline-block;
             text-decoration: none;
             color: white;
             padding: 1.25rem;
             border: .1875rem solid #0782fa;
+
+            &:hover {
+                background-color: #0782fa;
+                border: .1875rem solid white;
+            }
         }
 
     }
---
+++ src/components/footer/FooterSocialIcons.vue
@@ -0,0 +1,74 @@
+<script>
+export default {
+    name: "FooterSocial",
+    data() {
+        return {
+            socialIcons: [
+                {
+                    name: "Facebook",
+                    img: '/social-icons/footer-facebook.png'
+                },
+                {
+                    name: "Twitter",
+                    img: '/social-icons/footer-twitter.png'
+                },
+                {
+                    name: "Facebook",
+                    img: '/social-icons/footer-youtube.png'
+                },
+                {
+                    name: "Facebook",
+                    img: '/social-icons/footer-pinterest.png'
+                },
+                {
+                    name: "Facebook",
+                    img: '/social-icons/footer-periscope.png'
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+        <h4>FOLLOW US</h4>
+
+        <ul>
+            <li v-for="socialIcon in socialIcons">
+                <img :src="socialIcon.img" :alt="socialIcon.name">
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+div {
+    display: flex;
+    align-items: center;
+
+    h4 {
+        color: #0782fa;
+        font-size: 1.125rem;
+        margin-right: .625rem;
+        cursor: pointer;
+    }
+
+    ul {
+        display: flex;
+        list-style-type: none;
+
+        li {
+            img {
+                width: 3.125rem;
+
+                &:hover {
+                    cursor: pointer;
+                    background-color: #0782fa;
+                    border-radius: 50%;
+                }
+            }
+        }
+    }
+}
+</style>
