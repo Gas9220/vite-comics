962b39d5d46807a166ca7f5ab682fca110a82b99
Gaspare <Gas9220@users.noreply.github.com>
2023-03-24 18:04:47 +0100

    Aggiunto il jumbotron

--- src/components/main/Main.vue
+++ src/components/main/Main.vue
@@ -13,10 +13,20 @@ export default {
 
 <template>
     <main>
+        <section class="jumbo">
+        </section> 
         <NextFeatures></NextFeatures>
         <MainFeatures></MainFeatures>
     </main>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
+main {
+    section.jumbo {
+        background-image: url('/jumbotron.jpg');
+        height: 25rem;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+}
 </style>
